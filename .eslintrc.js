76ef1d2cd1fe1c2f2b6094cf08bf8bd5fdd71302
Wheat Carrier <wheatcarrier@gmail.com>
2020-09-21 04:56:29 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,32 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {}
-} else {
-  module.exports = {
-    env: {
-      browser: true,
-      es2020: true,
+const rules = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'react-app',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'plugin:react/recommended',
-      'react-app',
-      'airbnb',
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 11,
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
-    ],
-    rules: {
-      'linebreak-style': 'off',
-      'no-console': 'off',
-      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-      'react/prop-types': [0, {}],
-    },
-  }
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': 'off',
+    'no-console': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': [0, {}],
+  },
 }
+
+if (process.env.NODE_ENV !== 'production') {
+  rules.extends.push('airbnb');
+}
+
+module.exports = rules;
