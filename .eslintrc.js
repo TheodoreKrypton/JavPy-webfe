ded98240702a04332cfa232196ad4acc29bebc2c
Wheat Carrier <wheatcarrier@gmail.com>
2020-09-21 04:47:24 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,31 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'react-app',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {}
+} else {
+  module.exports = {
+    env: {
+      browser: true,
+      es2020: true,
     },
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    'linebreak-style': 'off',
-    'no-console': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/prop-types': [0, {}],
-  },
-};
+    extends: [
+      'plugin:react/recommended',
+      'react-app',
+      'airbnb',
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 11,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      'linebreak-style': 'off',
+      'no-console': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+      'react/prop-types': [0, {}],
+    },
+  }
+}
