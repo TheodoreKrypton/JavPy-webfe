e1748a9d42a490c3e97f246c8f185b9e237750db
Wheat Carrier <wheatcarrier@gmail.com>
2021-03-14 17:35:20 +0800

    fix iframe

--- src/pages/IFrame/index.js
+++ src/pages/IFrame/index.js
@@ -5,7 +5,13 @@ export default () => {
   const query = utils.useQuery();
 
   return (
-    <iframe title="video" src={query.get('video_url')} scrolling="no" frameBorder={0} width={700} height="430" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true">
+    <iframe
+      title="video"
+      src={query.get('video_url')}
+      style={{
+        width: '100vw', height: '100vh', margin: 0, padding: 0, border: 0,
+      }}
+    >
       <p>Your browser does not support iframes.</p>
     </iframe>
   );
